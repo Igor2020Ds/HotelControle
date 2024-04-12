c078e59ab3fe9b6cb0a75c87c7d41c58ac532007
IGOR <IGOR@IGOR>
2024-04-12 16:10:29 -0300

    Adicionar arquivos de projeto.

---
+++ VetorDesafio1/Estudante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Globalization;
+
+
+namespace Proj1
+{
+
+
+    public class Estudante
+    {
+
+        public string Email { get; set; }
+        public string Nome { get; set; }
+
+        public  Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;  
+        }
+
+        public override string ToString() {
+            return Nome + ' ' + Email;             
+        }
+        
+
+    }
+}
---
+++ VetorDesafio1/Program.cs
@@ -0,0 +1,61 @@
+﻿using Proj1;
+using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Dados
+{
+
+    class Prgram
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            Estudante[] vetor = new Estudante[10];
+
+            Console.WriteLine("Informe a quantidade de quartos que deseja alugar");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}");
+
+                Console.WriteLine($"Nome");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Email");
+                string email = Console.ReadLine();
+                Console.WriteLine("Número do Quarto");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Estudante(nome, email);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i]);
+                }
+
+                {
+
+
+
+                }
+
+            }
+
+
+
+
+        }
+
+    }
+}
+
